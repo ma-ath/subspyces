013474d82e32788945888a758f3b10f1e8e8f5ac
Matheus Lima <mlima@cvlab.cs.tsukuba.ac.jp>
2023-04-04 20:48:17 +0900

    Added pca method for VectorSet

--- subspaces/vector_set.py
+++ subspaces/vector_set.py
@@ -43,6 +43,13 @@ class VectorSet:
                 self.labels.append(label)
                 self.set[label] = VectorSubspace(vector_size=self.vector_size)
             self.set[label].append(vector)
+    
+    def pca(self, min_energy:float=0.8):
+        subset = VectorSet(labels=self.labels, vector_size=self.vector_size)
+        for label, subspace in self.set.items():
+            subsubspace = subspace.pca(min_energy)
+            subset.Populate(subsubspace.A, [label]*len(subsubspace))
+        return subset
 
 
 # --- unittests
@@ -79,7 +86,13 @@ class TestSubspaces(unittest.TestCase):
             mock_data = torch.rand(10, 24)
             mock_labels = list(range(10))
             set.Populate(mock_data, mock_labels)
-        
+    
+    def test_pca(self):
+        set = VectorSet(vector_size=32)
+        mock_data = torch.rand(100, 32)
+        mock_labels = [i%10 for i in list(range(100))]
+        set.Populate(mock_data, mock_labels)
+        subset = set.pca()
 
 if __name__ == "__main__":
     unittest.main()
