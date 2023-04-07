f49b84a86006d358a7a4dff5c8362b1e78d5f0be
Matheus Lima <mlima@cvlab.cs.tsukuba.ac.jp>
2023-04-07 15:06:59 +0900

    Calculating X on SMS cossine_similarity

--- subspaces/vector_msm.py
+++ subspaces/vector_msm.py
@@ -1,5 +1,7 @@
 import torch
 import unittest
+from itertools import groupby
+from typing import List
 
 from vector_space import VectorSpace
 from vector_set import VectorSet
@@ -9,72 +11,76 @@ class VectorMSM(VectorSM):
     """
     Class that implements a simple mutual subspace method for vector subspaces
     """
-    def eval(self, data:list(VectorSpace), correct_labels:list):
-        """
-        Simple Method that implements a common evaluation procedure for classification problems
-        Returns a list of correct classifications, and the success ratio
-        """
-        if data.dim() == 1:
-            data.unsqueeze_(0)
-        assert(data.shape[0] == len(correct_labels))
-        assert(data.shape[1] == self.vector_size)
+    # def eval(self, data:list(VectorSpace), correct_labels:list):
+    #     """
+    #     Simple Method that implements a common evaluation procedure for classification problems
+    #     Returns a list of correct classifications, and the success ratio
+    #     """
+    #     if data.dim() == 1:
+    #         data.unsqueeze_(0)
+    #     assert(data.shape[0] == len(correct_labels))
+    #     assert(data.shape[1] == self.vector_size)
         
-        predicted_labels = self.classify(data)
+    #     predicted_labels = self.classify(data)
         
-        correct_class = []
-        for l1, l2 in zip(predicted_labels, correct_labels):
-            correct_class.append(l1 == l2)
+    #     correct_class = []
+    #     for l1, l2 in zip(predicted_labels, correct_labels):
+    #         correct_class.append(l1 == l2)
         
-        prediction_ratio = correct_class.count(True) / len(correct_class)
+    #     prediction_ratio = correct_class.count(True) / len(correct_class)
         
-        return correct_class, prediction_ratio
+    #     return correct_class, prediction_ratio
     
-    def classify(self, vspace:list(VectorSpace)):
-        """
-        Classifies a tensor in one of the subspaces using the cossine similarity
-        """
-        if vectors.dim() == 1:
-            vectors.unsqueeze_(0)
-        assert(vectors.shape[1] == self.vector_size)
+    # def classify(self, vspace:list(VectorSpace)):
+    #     """
+    #     Classifies a tensor in one of the subspaces using the cossine similarity
+    #     """
+    #     if vectors.dim() == 1:
+    #         vectors.unsqueeze_(0)
+    #     assert(vectors.shape[1] == self.vector_size)
         
-        # Classify all vectors by cossine similarity
-        max_likelihood = [self.subset.labels[0]]*vectors.shape[0]
-        cs = [0]*vectors.shape[0]
+    #     # Classify all vectors by cossine similarity
+    #     max_likelihood = [self.subset.labels[0]]*vectors.shape[0]
+    #     cs = [0]*vectors.shape[0]
 
-        for subspace in self.subset:
-            foo = self.cossine_similarity(vectors, subspace)
-            for i in range(len(foo)):
-                if foo[i] > cs[i]: cs[i] = foo[i]; max_likelihood[i] = subspace.label
-        return max_likelihood
+    #     for subspace in self.subset:
+    #         foo = self.cossine_similarity(vectors, subspace)
+    #         for i in range(len(foo)):
+    #             if foo[i] > cs[i]: cs[i] = foo[i]; max_likelihood[i] = subspace.label
+    #     return max_likelihood
 
-    def cossine_similarity(self, vspaces:list(VectorSpace), subspace:VectorSpace):
+    def cossine_similarity(self, vspaces: List[VectorSpace], subspace:VectorSpace):
         """
         Returns the cossine similarity between a list of vector spaces and one subspace
         """
+        # Order list of vector spaces and group vector spaces of same size in batches
+        nlist = [space.n for space in vspaces]
+        z = zip(vspaces, nlist)
+        z = sorted(z)
+        sorted_vspaces = [i for i, _ in z]
+        sorted_nlist = [i for _, i in z]
+        group_nlist = [len(list(n)) for _, n in groupby(sorted_nlist)]
 
-        
+        # Group vector spaces in 3d tensors, calculate cossine similarities in one go
+        i = 0
+        for n in group_nlist:
+            vspace_tensor = torch.vstack([vspace.A.unsqueeze(0) for vspace in sorted_vspaces[i:i+n]])
+            # vspace_tensor.shape = [vspace, vspace.n, vspace.vector_size]
+            batch_subspace = subspace.A.unsqueeze(0).repeat(n, 1, 1)
 
-        assert(vector.shape[1] == subspace.vector_size)
-        vector = vector.type(torch.FloatTensor)
-
-        S = torch.sum(
-                torch.div(
-                    torch.mm(vector, subspace.A.t())**2,
-                    torch.matmul(
-                        torch.sqrt(
-                            torch.diag(
-                                torch.mm(vector, vector.t()
-                                )
-                            )
-                        ).unsqueeze(0).t(),
-                        torch.sqrt(
-                            torch.diag(
-                                torch.mm(subspace.A, subspace.A.t())
-                            )
-                        ).unsqueeze(0)
-                    )
-                ), dim=1
+            X = torch.bmm(
+                    torch.bmm(batch_subspace, vspace_tensor.transpose(2, 1)),
+                    torch.bmm(batch_subspace, vspace_tensor.transpose(1, 2)).transpose(1, 2)
             )
+            print("subspace", batch_subspace.shape)
+            print("tensor", vspace_tensor.shape)
+            print("X", X.shape)            
+
+            i += n
+
+        S = 0
+
+        # Unsort cossine similarities
         return S
 
 
@@ -84,39 +90,44 @@ class TestVectorSM(unittest.TestCase):
         pass
 
     def test_cossine_similarity(self):
-        sm = VectorSM()
+        msm = VectorMSM()
         subspace = VectorSpace(vector_size=2)
-        subspace.append(torch.tensor([1, 0]))
-        vector = torch.tensor([0, 1])
-        self.assertTrue(torch.allclose(sm.cossine_similarity(vector, subspace), torch.zeros(1)))
-        vector = torch.tensor([1, 0])
-        self.assertTrue(torch.allclose(sm.cossine_similarity(vector, subspace), torch.ones(1)))
-        vector = torch.tensor([[0, 2], [1, 0]])
-        sm.cossine_similarity(vector, subspace)
+        subspace.append(torch.tensor([[0, 1], [1, 0]]))
+        vspace12 = VectorSpace(vector_size=2)
+        vspace12.append(torch.tensor([[1, 1]]))
+        vspace22 = VectorSpace(vector_size=2)
+        vspace22.append(torch.tensor([[-1, 1], [1, -1]]))
+        vspace32 = VectorSpace(vector_size=2)
+        vspace32.append(torch.tensor([[-1, 1], [1, -1], [0, 1]]))
 
-    def test_train(self):
-        sm = VectorSM(vector_size=32)
-        mock_data = torch.rand(100, 32)
-        mock_labels = [i%10 for i in list(range(100))]
-        sm.train(mock_data, mock_labels)
+        test_list = [vspace22, vspace32, vspace22, vspace32, vspace12, vspace12, vspace12]
+
+        similarity = msm.cossine_similarity(test_list, subspace)
+
+
+    # def test_train(self):
+    #     sm = VectorSM(vector_size=32)
+    #     mock_data = torch.rand(100, 32)
+    #     mock_labels = [i%10 for i in list(range(100))]
+    #     sm.train(mock_data, mock_labels)
     
-    def test_classify(self):
-        sm = VectorSM(vector_size=32)
-        mock_data = torch.rand(100, 32)
-        mock_labels = [i%10 for i in list(range(100))]
-        sm.train(mock_data, mock_labels)
-        mock_vector = torch.rand(10, 32)
-        labels = sm.classify(mock_vector)
-        self.assertEqual(len(labels), 10)
+    # def test_classify(self):
+    #     sm = VectorSM(vector_size=32)
+    #     mock_data = torch.rand(100, 32)
+    #     mock_labels = [i%10 for i in list(range(100))]
+    #     sm.train(mock_data, mock_labels)
+    #     mock_vector = torch.rand(10, 32)
+    #     labels = sm.classify(mock_vector)
+    #     self.assertEqual(len(labels), 10)
     
-    def test_eval(self):
-        sm = VectorSM(vector_size=32)
-        mock_data = torch.rand(100, 32)
-        mock_labels = [i%10 for i in list(range(100))]
-        sm.train(mock_data, mock_labels)
-        mock_vector = torch.rand(10, 32)
-        mock_labels = [i for i in list(range(10))]
-        eval = sm.eval(mock_vector, mock_labels)
+    # def test_eval(self):
+    #     sm = VectorSM(vector_size=32)
+    #     mock_data = torch.rand(100, 32)
+    #     mock_labels = [i%10 for i in list(range(100))]
+    #     sm.train(mock_data, mock_labels)
+    #     mock_vector = torch.rand(10, 32)
+    #     mock_labels = [i for i in list(range(10))]
+    #     eval = sm.eval(mock_vector, mock_labels)
 
 
 if __name__ == "__main__":
