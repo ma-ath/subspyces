0788d33a3e677fa569724b5f6c8643994d2f009b
Matheus Lima <mlima@cvlab.cs.tsukuba.ac.jp>
2024-04-11 19:52:23 +0900

    Added some things

--- subspaces/tests/structure_similarity_test.py
+++ subspaces/tests/structure_similarity_test.py
@@ -13,29 +13,25 @@ from subspaces.transform import PCATransform
 class TestPCATransform(unittest.TestCase):
     def setUp(self):
         # dataset = FakeData(64, [32, 32], 2,
-        #                    transform=T.Compose([T.PILToTensor(), torch.flatten]))
-        self.data = dict()
+        #                    transform=T.Compose([T.ToTensor(), torch.flatten]))
         dataset = MNIST("~/datasets", download=True, train=False,
-                        transform=T.Compose([T.PILToTensor(), torch.flatten]))
+                        transform=T.Compose([T.ToTensor(), torch.flatten]))
         generator = IdentityGenerator()
         vector_spaces = generator.generate(dataset, batch_size=32)
         pca_transform = PCATransform(n_components=10)
+        self.data = dict()
         for vector_space in vector_spaces.values():
             self.data[vector_space.label] = pca_transform.transform(vector_space)
 
     def is_psd(self, mat):
-        print(torch.allclose(mat, mat.T))
-        print((torch.linalg.eigvals(mat)))
-        return bool((mat == mat.T).all() and (torch.linalg.eigvals(mat).real >= 0).all())
+        return bool(torch.allclose(mat, mat.T) and (torch.linalg.eigvals(mat).real >= 0).all())
 
     def test_structure_similarity(self):
         gram_matrix = torch.zeros(10, 10)
-        print(self.data[0])
-        # for i in range(1, 10):
-        #     for j in range(1, 10):
-        #         gram_matrix[i][j] = structure_similarity(self.data[i], self.data[j])
-
-        # print(self.is_psd(gram_matrix))
+        for i in range(1, 10):
+            for j in range(1, 10):
+                gram_matrix[i][j] = structure_similarity(self.data[i], self.data[j])
+        self.assertTrue(self.is_psd(gram_matrix))
 
 
 if __name__ == "__main__":
--- subspaces/transform/pca_transform.py
+++ subspaces/transform/pca_transform.py
@@ -1,6 +1,8 @@
 import torch
 from torch import linalg
 from sklearn.decomposition import PCA
+import warnings
+from warnings import WarningMessage
 
 from .abstract_transform import AbstractTransform
 from subspaces import VectorSpace
@@ -17,6 +19,7 @@ class PCATransform(AbstractTransform):
             self._pca_transform = PCA(n_components=self.n_components,
                                       copy=False,
                                       **kwargs)
+        self._etol = 1e-6
 
     def __str__(self) -> str:
         return "PCATransform"
@@ -27,9 +30,30 @@ class PCATransform(AbstractTransform):
         else:
             auto_correlation_matrix = vector_space._data.T.conj() @ vector_space._data
             auto_correlation_matrix /= (vector_space.n-1)
-            eigenvalues, eigenvectors = linalg.eigh(auto_correlation_matrix)
+            eigenvalues, eigenvectors = linalg.eig(auto_correlation_matrix)
+            #  Eigenvalues are supposed to be positive reals.
+            # This condtion may fail only if there are numerical errors. 
+            # We can ignore the torch warning here about casting complex -> real
+            if torch.max(torch.imag(eigenvalues)) > self._etol:
+                raise (AssertionError(
+                    "Eigenvalues of autocorrelation matrix are supposed to be real," +
+                    f"but has imaginary part {torch.max(torch.imag(eigenvalues))}"))
+
+            # TODO: This filter is not working...
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                eigenvalues = torch.real(eigenvalues)
+
             sorted_indices = torch.argsort(eigenvalues, descending=True)
-            pca_ = eigenvectors[sorted_indices, :][1:self.n_components+1]
+            pca_ = eigenvectors[sorted_indices, :][:self.n_components]
+
+            # Check the eigenvectors for NaN. An NaN can show there was a numerical error
+            if torch.isnan(pca_).any():
+                warnings.warn("A NaN was generated while calculating the eigenvectors." +
+                              "This may suggest a numerical error on calculation." +
+                              "Casting all NaN to 0.", RuntimeWarning)
+
+                pca_ = torch.nan_to_num(pca_)
 
         new_vector_space = VectorSpace(dim=vector_space.dim, label=vector_space.label)
         new_vector_space.append(pca_)
