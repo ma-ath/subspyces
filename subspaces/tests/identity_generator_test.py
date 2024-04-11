6944271ae8e363f5aaa62e6e2daad7d6d7684133
Matheus Lima <mlima@cvlab.cs.tsukuba.ac.jp>
2024-04-11 02:15:41 +0000

    simple fix

--- subspaces/tests/identity_generator_test.py
+++ subspaces/tests/identity_generator_test.py
@@ -1,9 +1,9 @@
 import unittest
 import torch
 import torchvision.transforms as T
-from torchvision.datasets import MNIST, FakeData
+from torchvision.datasets import FakeData
+# from torchvision.datasets import MNIST
 
-# from subspaces import VectorSpace
 from subspaces.generators import IdentityGenerator
 
 
@@ -11,14 +11,13 @@ from subspaces.generators import IdentityGenerator
 class TestIdentityGenerator(unittest.TestCase):
     def setUp(self):
         self.generator = IdentityGenerator()
-        self.dataset = MNIST("~/datasets", download=True, train=False,
-                             transform=T.Compose([T.PILToTensor(), torch.flatten]))
-        # self.dataset = FakeData(64, [32, 32], 2,
-        #                         transform=T.Compose([T.PILToTensor(), torch.flatten]))
+        # self.dataset = MNIST("~/datasets", download=True, train=False,
+        #                      transform=T.Compose([T.PILToTensor(), torch.flatten]))
+        self.dataset = FakeData(64, [32, 32], 2,
+                                transform=T.Compose([T.PILToTensor(), torch.flatten]))
 
     def test_generate(self):
-        subspace_list = self.generator.generate(self.dataset, batch_size=32)
-        print(subspace_list)
+        _ = self.generator.generate(self.dataset, batch_size=32)
 
 
 if __name__ == "__main__":
