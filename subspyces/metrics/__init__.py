e1ad8bc5c4d022a6facc603a5cf2b7aa9e3a0a5a
Matheus Lima <mlima@cvlab.cs.tsukuba.ac.jp>
2024-04-16 16:44:03 +0900

    Still a lot of work to be done

--- subspyces/metrics/__init__.py
+++ subspyces/metrics/__init__.py
@@ -1,6 +1,7 @@
 from .structure_similarity import structure_similarity
 from .cosine_similarity import cosine_similarity
 
+
 __all__ = ["structure_similarity",
            "cosine_similarity",
            ]
--- subspyces/metrics/cosine_similarity.py
+++ subspyces/metrics/cosine_similarity.py
@@ -1,12 +1,39 @@
 import torch
 from torch import linalg
-from torch.nn import functional as F
 from typing import Union
 import numpy as np
 
 from subspyces import VectorSpace
 
 
-def cosine_similarity(x: Union[torch.Tensor, np.ndarray],
-                      A: VectorSpace):
-    pass
+def cosine_similarity(x: Union[torch.Tensor, np.ndarray, VectorSpace],
+                      y: Union[torch.Tensor, np.ndarray, VectorSpace]) -> torch.Tensor:
+    raise (NotImplementedError)
+    """
+    Returns S = \sum_{i=0}^{r-1} \frac{(x,\phi_i)^2}{\|x\|\|\phi_i\|}
+    """
+    if vector.dim() == 1:
+        vector.unsqueeze_(0)
+    assert(vector.shape[1] == subspace.vector_size)
+    vector = vector.type(torch.FloatTensor)
+
+    S = torch.sum(
+            torch.div(
+                torch.mm(vector, subspace.A.t())**2,
+                torch.matmul(
+                    torch.sqrt(
+                        torch.diag(
+                            torch.mm(vector, vector.t()
+                            )
+                        )
+                    ).unsqueeze(0).t(),
+                    torch.sqrt(
+                        torch.diag(
+                            torch.mm(subspace.A, subspace.A.t())
+                        )
+                    ).unsqueeze(0)
+                )
+            ), dim=1
+        )
+    print(S)
+    return S
