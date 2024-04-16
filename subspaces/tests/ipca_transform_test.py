8cf8a9a1a5f8aa2f53ec5ef4b068bbde1642233e
Matheus Lima <mlima@cvlab.cs.tsukuba.ac.jp>
2024-04-16 11:11:48 +0900

    Started writting somethings

--- subspaces/tests/ipca_transform_test.py
+++ subspaces/tests/ipca_transform_test.py
@@ -5,6 +5,7 @@ import torchvision.transforms as T
 from torchvision.datasets import MNIST
 
 from subspaces.transform import IncrementalPCATransform
+from subspaces.transform import PCATransform
 from subspaces.generators import IdentityGenerator
 
 
@@ -20,10 +21,24 @@ class TestPCATransform(unittest.TestCase):
         self.vector_spaces = generator.generate(dataset, batch_size=32)
 
     def test_transform(self):
-        pca_transform = IncrementalPCATransform(n_components=10, use_sklearn=True)
-        for vector_space in self.vector_spaces.values():
-            _ = pca_transform.transform(vector_space)
-            print(_)
+        for use_sklearn in [True]:  # , False]:
+            pca_transform = IncrementalPCATransform(n_components=10, batch_size=32,
+                                                    use_sklearn=use_sklearn)
+            for vector_space in self.vector_spaces.values():
+                _ = pca_transform.transform(vector_space)
+
+    def test_pca_ipca_close(self):
+        etol = 1e-4
+        for use_sklearn in [True]:  # , False]:
+            ipca_transform = IncrementalPCATransform(n_components=10, batch_size=None,
+                                                     use_sklearn=use_sklearn)
+            pca_transform = PCATransform(n_components=10, use_sklearn=use_sklearn)
+            for vector_space in self.vector_spaces.values():
+                pca = pca_transform.transform(vector_space)
+                ipca = ipca_transform.transform(vector_space)
+                error = torch.abs(torch.abs(pca._data) - torch.abs(ipca._data)).mean()
+                print(error)
+                self.assertTrue(error < etol)
 
 
 if __name__ == "__main__":
--- subspaces/transform/ipca_transform.py
+++ subspaces/transform/ipca_transform.py
@@ -2,6 +2,7 @@ import torch
 from torch import linalg
 from sklearn.decomposition import IncrementalPCA
 import warnings
+from typing import Union
 
 from .abstract_transform import AbstractTransform
 from subspaces import VectorSpace
@@ -11,14 +12,20 @@ class IncrementalPCATransform(AbstractTransform):
     """
     Applies the sklearn.decomposition.PCA onto a VectorSpace
     """
-    def __init__(self, n_components: int, use_sklearn: bool = False, **kwargs):
-        self.use_sklearn = use_sklearn
+    def __init__(self, n_components: int, batch_size: Union[int, None],
+                 use_sklearn: bool = False, **kwargs) -> None:
         self.n_components = n_components
+        self.batch_size = batch_size
+        self.use_sklearn = use_sklearn
         if self.use_sklearn:
             self._ipca_transform = IncrementalPCA(n_components=self.n_components,
+                                                  batch_size=self.batch_size,
                                                   copy=False,
                                                   **kwargs)
-        self._etol = 1e-6
+
+        # self.etol_ = 1e-6
+        # self.components_ = None
+        # self.batch_size_ = None
 
     def __str__(self) -> str:
         return "IncrementalPCATransform"
@@ -29,13 +36,22 @@ class IncrementalPCATransform(AbstractTransform):
                           "Make sure you know what you are doing", UserWarning)
             ipca_ = self._ipca_transform.fit(vector_space._data).components_.copy().T
         else:
+            raise (NotImplementedError)
+            # Code based on sklearn.decomposition.IncrementalPCA
+            # Choose batch_size
+            _, n_features = vector_space._data
+            if self.batch_size is None:
+                self.batch_size_ = 5 * n_features
+            else:
+                self.batch_size_ = self.batch_size
+
             auto_correlation_matrix = torch.matmul(vector_space._data.H, vector_space._data)
             auto_correlation_matrix /= (vector_space.n-1)
             eigenvalues, eigenvectors = linalg.eig(auto_correlation_matrix)
             #  Eigenvalues are supposed to be positive reals.
             # This condtion may fail only if there are numerical errors. 
             # We can ignore the torch warning here about casting complex -> real
-            if torch.max(torch.imag(eigenvalues)) > self._etol:
+            if torch.max(torch.imag(eigenvalues)) > self.etol_:
                 raise (AssertionError(
                     "Eigenvalues of autocorrelation matrix are supposed to be real," +
                     f"but has imaginary part {torch.max(torch.imag(eigenvalues))}"))
