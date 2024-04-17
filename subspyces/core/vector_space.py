Making sure cosine_similarity calculation is correct

subspyces/core/vector_space.py
@@ -35,13 +35,14 @@ class VectorSpace:
             raise (IndexError("Index i out of bound"))
         return self._data[i]
 
-    def to(self, device: Union[torch.device, str]) -> None:
+    def to(self, device: Union[torch.device, str]) -> "VectorSpace":
         """
         Sends vector space to specific device
         """
         self._data.to(device=device)
+        return self
 
-    def append(self, vector: Union[torch.Tensor, np.ndarray]) -> None:
+    def append(self, vector: Union[torch.Tensor, np.ndarray]) -> "VectorSpace":
         """
         Appends vectors to the basis vectors of vector space
         """
@@ -57,6 +58,7 @@ class VectorSpace:
         vector = vector.type(self.dtype)
         self.n = self.n + vector.shape[0]
         self._data = torch.cat([self._data, vector], dim=0)
+        return self
 
     def __lt__(self, other) -> int:
         return self.n < other.n

subspyces/metrics/cosine_similarity.py
@@ -1,5 +1,5 @@
 import torch
-from torch import functional as F
+from torch.nn import functional as F
 from typing import Union
 import numpy as np
 
@@ -12,6 +12,8 @@ def cosine_similarity(x: Union[torch.Tensor, np.ndarray, VectorSpace],
     Returns the cosine similarity between basis vectors of subspaces
     :math:`cs = \frac{(\phi_i,\psi_j)}{\|\phi_i\|\|\psi_j\|}`
     """
+    # NOTE: These many checks can slow down computation. Maybe there is a better way to
+    # implement this generically (maybe just ignore the checks?).
     if (not isinstance(x, (torch.Tensor, np.ndarray, VectorSpace)) or
             not isinstance(y, (torch.Tensor, np.ndarray, VectorSpace))):
         raise (TypeError("Invalid input type!"))
@@ -26,10 +28,15 @@ def cosine_similarity(x: Union[torch.Tensor, np.ndarray, VectorSpace],
     if x.dim() > 2 or y.dim() > 2:
         raise (RuntimeError(("input cannot have more then 2 dimensions, "
                             f"but is has {x.dim()} and {y.dim()} dimensions.")))
+    if x.dim() == 1:
+        x.unsqueeze_(0)
+    if y.dim() == 1:
+        y.unsqueeze_(0)
 
     x = x.type(torch.FloatTensor)
     y = y.type(torch.FloatTensor)
 
     cosine_similarity = torch.matmul(F.normalize(x),
                                      F.normalize(y).H)
+    cosine_similarity.squeeze_()
     return cosine_similarity

subspyces/metrics/structure_similarity.py
@@ -1,6 +1,4 @@
 import torch
-from torch import linalg
-from torch.nn import functional as F
 from typing import Union
 import numpy as np
 
@@ -9,7 +7,7 @@ from .canonical_angles import canonical_angles
 
 
 def structure_similarity(A: Union[torch.Tensor, np.ndarray, VectorSpace],
-                         B: Union[torch.Tensor, np.ndarray, VectorSpace]) -> float:
+                         B: Union[torch.Tensor, np.ndarray, VectorSpace]) -> torch.Tensor:
     r"""
     Structure similarity between two vector spaces.
     This returns the mean of squared cosines of the canonical angles between
@@ -17,4 +15,4 @@ def structure_similarity(A: Union[torch.Tensor, np.ndarray, VectorSpace],
     are calculated using the SVD of the similarity matrix.
     """
     ca = canonical_angles(A, B)
-    return float(torch.mean(torch.square(ca)))
+    return torch.mean(torch.square(ca))

subspyces/tests/metrics_cosine_similarity_test.py
@@ -1,37 +1,63 @@
 import unittest
+import numpy as np
 import torch
-import torchvision.transforms as T
-# from torchvision.datasets import FakeData
-from torchvision.datasets import MNIST
+import math
 
-from subspyces.metrics import structure_similarity
-from subspyces.generators import IdentityGenerator
-from subspyces.transform import PCATransform
+from subspyces.metrics import cosine_similarity
+from subspyces import VectorSpace
 
 
 # --- unittests
-class TestPCATransform(unittest.TestCase):
+class TestMetricsCosineSimilarity(unittest.TestCase):
     def setUp(self):
-        # dataset = FakeData(64, [32, 32], 2,
-        #                    transform=T.Compose([T.ToTensor(), torch.flatten]))
-        dataset = MNIST("~/datasets", download=True, train=False,
-                        transform=T.Compose([T.ToTensor(), torch.flatten]))
-        generator = IdentityGenerator()
-        vector_spaces = generator.generate(dataset, batch_size=32)
-        pca_transform = PCATransform(n_components=10)
-        self.data = dict()
-        for vector_space in vector_spaces.values():
-            self.data[vector_space.label] = pca_transform.transform(vector_space)
+        self.A = np.array([
+            [10, 0, 0, 0],  # First basis
+            [0, 15, 0, 0],  # Second basis
+            [5, 5, 0, 0]   # Third basis
+        ])
+        self.B = np.array([
+            [0, 0, 8, 0],  # First basis
+            [0, 7, 0, 0],  # Second basis
+            [0, 19, 19, 0]   # Third basis
+        ])
+        self.res1 = torch.tensor([
+            [0, 0, 0],
+            [0, 1, math.sqrt(2)/2],
+            [0, math.sqrt(2)/2, 0.5]
+        ])
+        self.C = np.array(
+            [1, 0, 0, 0]
+        )
+        self.res2 = torch.tensor(
+            [1, 0, math.sqrt(2)/2]
+        )
+        self.subspaceA = VectorSpace(4).append(self.A)
+        self.subspaceB = VectorSpace(4).append(self.B)
 
-    def is_psd(self, mat):
-        return bool(torch.allclose(mat, mat.H) and (torch.linalg.eigvals(mat).real >= 0).all())
+    def test_input_checks(self):
+        with self.assertRaises(TypeError):
+            cosine_similarity(self.A, None)
+        with self.assertRaises(TypeError):
+            cosine_similarity(None, self.B)
+        with self.assertRaises(RuntimeError):
+            cosine_similarity(torch.rand(1, 2, 2), torch.rand(1, 2, 2))
 
-    def test_structure_similarity(self):
-        gram_matrix = torch.zeros(10, 10)
-        for i in range(0, 10):
-            for j in range(0, 10):
-                gram_matrix[i][j] = structure_similarity(self.data[i], self.data[j])
-        self.assertTrue(self.is_psd(gram_matrix))
+    def test_can_calculate(self):
+        _ = cosine_similarity(torch.rand(2, 2), torch.rand(2, 2))
+        _ = cosine_similarity(np.random.rand(2, 2), np.random.rand(2, 2))
+        _ = cosine_similarity(self.subspaceA, self.subspaceB)
+        _ = cosine_similarity(torch.rand(2), torch.rand(2, 2))
+        pass
+
+    def test_calculation_is_correct(self):
+        cs = cosine_similarity(self.A, self.B)
+        self.assertTrue(torch.allclose(cs, self.res1))
+        cs = cosine_similarity(self.B, self.A)
+        self.assertTrue(torch.allclose(cs, self.res1))
+        cs = cosine_similarity(self.A, self.C)
+        self.assertTrue(torch.allclose(cs, self.res2))
+        cs = cosine_similarity(self.C, self.A)
+        self.assertTrue(torch.allclose(cs, self.res2))
 
 
 if __name__ == "__main__":